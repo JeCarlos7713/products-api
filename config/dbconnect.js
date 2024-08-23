de1249b10e5fbe9a75a6ac9c60a543158765bddc
Jean Carlos <contato.jeancl25@gmail.com>
2024-08-23 17:56:37 -0300

    feat: conexão com o banco de dados

---
+++ config/dbconnect.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './../db/products.db'
+});
+
+try {
+    await sequelize.authenticate()
+    console.log("Banco de dados conectado!")
+} catch (error) {
+    console.log("Erro na autenticação com o banco: " + error)
+    throw error
+}
+
+export default sequelize
